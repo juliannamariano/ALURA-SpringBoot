882dc9689fc33f79a6afa182b9d22f7e52f7e9c7
Julianna Mariano <juliannamariano27@outlook.com>
2025-04-04 19:09:21 -0300

    Primeiro "Hello-world" spring boot

---
+++ api/src/main/java/med/voll/api/HelloController.java
@@ -0,0 +1,20 @@
+//Pacote que recebe as requisições tanto do back-end quanto do front
+package med.voll.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*Para informar que essa classe é um controller colocamos essa anotação (Ele é uma classe "RestController"*/
+@RestController
+@RequestMapping("/hello") //Nessa anotação indica qual URL que nós vamos responder
+public class HelloController {
+
+    /*Essa anotação indica de qual método HTTP que ele está respondendo*/
+    @GetMapping  //Toda vez que for chamado o método "get" vai voltar esse método abaixo
+    public String olaMundo(){
+        return "Hello Julianna";
+    }
+
+
+}
